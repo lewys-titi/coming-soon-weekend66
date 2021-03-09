216a1c8ca8df1d83ec169fb82711b8d1bcfd0699
Lucas <lucas.moquet95@gmail.com>
2021-03-09 14:09:17 +0100

    page with bg

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Text from './Text';
+
+function App() {
+  return (
+   <div><Text /></div>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'balsamiq': ['Balsamiq Sans', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      backgroundImage: theme => ( {
+        'upcoming':"url('/src/assets/luxury-bedroom-suite.jpg')",
+      })
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
