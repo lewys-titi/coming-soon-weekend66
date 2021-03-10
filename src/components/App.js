dbb938cf6591f5ceb8d9611018e4b865ad59e38c
Lucas <lucas.moquet95@gmail.com>
2021-03-10 12:36:05 +0100

    bg with opacity

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Text from './Text';
 
 function App() {
   return (
-   <main className="bg-black bg-opacity-90"><Text /></main>
+   <main><Text /></main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'montserrat': ['Montserrat', 'Arial', 'sans-serif'],
       },
       backgroundImage: theme => ( {
-        'upcoming':"url('/src/assets/luxury-bedroom-suite.jpg')",
+        'upcoming':"url('/src/assets/luxury-bedroom-suite-opacity-90.jpg')",
       }),
       maxWidth: {
         'xxs': '10rem',
