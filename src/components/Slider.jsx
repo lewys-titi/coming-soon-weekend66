009fa19016dee0b053b7bc1e7f74fe2ea305f4f7
Lucas <lucas.moquet95@gmail.com>
2021-03-24 17:43:49 +0100

    autoplay & fade ok

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,4 @@
-import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
+import SwiperCore, { Navigation, Pagination, A11y, EffectFade, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import luxury from '../assets/luxury.jpg'; 
 import illu2 from '../assets/illu2.jpg';
@@ -9,22 +9,25 @@ import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
+import 'swiper/components/effect-fade/effect-fade.scss';
 
 // install Swiper modules
-SwiperCore.use([Navigation, Pagination, A11y]);
+SwiperCore.use([Navigation, Pagination, A11y, EffectFade, Autoplay]);
 
 export default function Slider(){
   
   return (
     
     <Swiper
-      spaceBetween={0}
+      effect="fade"
+      spaceBetween={30}
       slidesPerView={1}
-      navigation
       pagination={{ clickable: true }}
+      autoplay={{delay: 2500, disableOnInteraction: false}}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
       className="min-w-full overflow-hidden"
+      loop={true}
     >
       <SwiperSlide key="1" className="h-full"><img src={luxury} alt="test" className=" h-full object-cover"/></SwiperSlide>
       <SwiperSlide key="2" className="h-full"><img src={illu2} alt="test" className="h-full object-cover"/></SwiperSlide>
